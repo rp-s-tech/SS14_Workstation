8147f2de09b847521bf2b05afcb4df6729503a92
Qwerty <qwerty@qwerty.qwerty>
2024-12-22 18:38:46 +0700

    Adding migrations for Barks (by FaDeOkno)

---
+++ Content.Server.Database/Migrations/Sqlite/20241222111109_Barks.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class Barks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "bark_pitch",
+                table: "profile",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<string>(
+                name: "bark_proto",
+                table: "profile",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<float>(
+                name: "high_bark_var",
+                table: "profile",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "low_bark_var",
+                table: "profile",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "bark_pitch",
+                table: "profile");
+
+            migrationBuilder.DropColumn(
+                name: "bark_proto",
+                table: "profile");
+
+            migrationBuilder.DropColumn(
+                name: "high_bark_var",
+                table: "profile");
+
+            migrationBuilder.DropColumn(
+                name: "low_bark_var",
+                table: "profile");
+        }
+    }
+}
