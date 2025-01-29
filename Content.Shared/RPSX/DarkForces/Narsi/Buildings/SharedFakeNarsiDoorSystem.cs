4c4fdc65eadef17b8c4965f66fb91c2ba92ff190
Aristophan <taksak426@gmail.com>
2025-01-29 14:01:58 +0000

    Я пофиксил 20 багов...

--- Content.Shared/RPSX/DarkForces/Narsi/Buildings/SharedFakeNarsiDoorSystem.cs
+++ Content.Shared/RPSX/DarkForces/Narsi/Buildings/SharedFakeNarsiDoorSystem.cs
@@ -1,5 +1,6 @@
 using Robust.Shared.GameStates;
 using Robust.Shared.Serialization;
+using Content.Shared.RPSX.DarkForces.Narsi.Roles;
 
 namespace Content.Shared.RPSX.DarkForces.Narsi.Buildings;
 
@@ -8,12 +9,23 @@ public abstract class SharedFakeNarsiDoorSystem : EntitySystem
     public override void Initialize()
     {
         base.Initialize();
+        SubscribeNetworkEvent<FakeDoorCheckPlayerEvent>(OnFakeFoorChecked);
         SubscribeLocalEvent<SharedFakeNarsiDoorComponent, ComponentGetState>(OnFakeNarsiDoorGetState);
     }
 
     private void OnFakeNarsiDoorGetState(EntityUid uid, SharedFakeNarsiDoorComponent component, ref ComponentGetState args)
     {
         args.State = new SharedFakeNarsiDoorComponentState(component.FakeRsiPath, component.RealRsiPath);
+    }
+
+    private void OnFakeFoorChecked(FakeDoorCheckPlayerEvent args)
+    {
+        if (HasComp<NarsiCultistComponent>(GetEntity(args.Entity)))
+        {
+            args.IsCultist = true;
+            return;
+        }
+        args.IsCultist = false;
     }
 }
 
@@ -26,5 +38,18 @@ public sealed class SharedFakeNarsiDoorComponentState : ComponentState
     {
         FakeRsiPath = fakeRsiPath;
         RealRsiPath = realRsiPath;
+    }
+}
+
+[Serializable, NetSerializable]
+public sealed class FakeDoorCheckPlayerEvent : EntityEventArgs
+{
+    public NetEntity Entity;
+    public bool IsCultist;
+
+    public FakeDoorCheckPlayerEvent(NetEntity entity, bool cultist = false)
+    {
+        Entity = entity;
+        IsCultist = cultist;
     }
 }
