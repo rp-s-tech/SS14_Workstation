c2390834ec958ab285a1ebdbf525d3ae522586a0
Aristophan <taksak426@gmail.com>
2025-02-28 16:55:10 +0400

    хирургия. начало х2

---
+++ Content.Client/RPSX/Bridges/ITargetDollWidgetBridge.cs
@@ -0,0 +1,43 @@
+using Content.Shared.Body.Part;
+using Robust.Client.UserInterface;
+
+namespace Content.Client.RPSX.Bridges;
+
+public interface ITargetDollWidgetBridge
+{
+    public void SelectBodyPart(BodyPartType? targetBodyPart, BodyPartSymmetry bodyPartSymmetry);
+    public void Clear();
+    public void Hide();
+    public void Show();
+    public void InitializeWidget();
+    public void SetupWidget(Control surface);
+}
+
+public sealed class StubTargetDollWidgetBridge : ITargetDollWidgetBridge
+{
+    public void SelectBodyPart(BodyPartType? targetBodyPart, BodyPartSymmetry bodyPartSymmetry)
+    {
+    }
+
+    public void Clear()
+    {
+
+    }
+
+    public void Hide()
+    {
+    }
+
+    public void Show()
+    {
+    }
+
+    public void InitializeWidget()
+    {
+    }
+
+    public void SetupWidget(Control surface)
+    {
+
+    }
+}
---
+++ Content.Client/RPSX/Medical/MedicalDropperVisualsComponent.cs
@@ -0,0 +1,19 @@
+﻿namespace Content.Client.SecretStation.Medical;
+
+[RegisterComponent]
+public sealed partial class MedicalDropperVisualsComponent : Component;
+
+public enum MedicalDropperVisualLayers : byte
+{
+    Empty,
+    WithBottle
+}
+
+public enum MedicalDropperBottleVisualLayers : byte
+{
+    Empty,
+    Full,
+    ThreeQuarters,
+    Half,
+    OneQuarters,
+}
