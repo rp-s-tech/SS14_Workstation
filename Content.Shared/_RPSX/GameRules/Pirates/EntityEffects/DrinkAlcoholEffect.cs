3bd483ab1fda05b3a5fe8a2dd8f66764698147a4
Aristophan_ <118120220+aristophanivan@users.noreply.github.com>
2025-07-05 20:12:52 +0400

    pirates prodolzhenie

---
+++ Content.Shared/_RPSX/GameRules/Pirates/EntityEffects/DrinkAlcoholEffect.cs
@@ -0,0 +1,13 @@
+﻿using Content.Shared.EntityEffects;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.RPSX.GameRules.Pirates.EntityEffects;
+
+[DataDefinition]
+public sealed partial class DrinkAlcoholEffect : EventEntityEffect<DrinkAlcoholEffect>
+{
+    protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
+    {
+        return "";
+    }
+}
